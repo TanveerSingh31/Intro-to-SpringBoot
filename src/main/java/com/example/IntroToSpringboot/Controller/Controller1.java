fc44b625d5aded67f2c2312a6cab12da9b709637
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-05-07 23:14:36 +0530

    profiling in springboot

--- src/main/java/com/example/IntroToSpringboot/Controller/Controller1.java
+++ src/main/java/com/example/IntroToSpringboot/Controller/Controller1.java
@@ -2,12 +2,16 @@ package com.example.IntroToSpringboot.Controller;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Profile("prod")
 @RestController
 public class Controller1 {
 
@@ -21,6 +25,8 @@ public class Controller1 {
     @Autowired
     RequestScopeController requestScopeControllerObj = null;
 
+    @Value("${env_data}")
+    Object obj;
 
     // ConditionalOnProperty -> conditionally creates beans
     @Autowired(required = false)
@@ -44,6 +50,8 @@ public class Controller1 {
         System.out.println("singleton obj hash = " + singletonControllerObj.hashCode());
         System.out.println("prototype obj hash = " + prototypeControllerObj.hashCode());
         System.out.println("bean of ConditionalOnProperty = " + conditionalOnPropertyClassObj);
+        System.out.println("env property = " + obj);
+
     }
 }
 
